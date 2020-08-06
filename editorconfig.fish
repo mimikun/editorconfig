c17de6fd34f2c91ae7883fc480eb6c9019eac55b
Yuto Tanaka <karaage_nennen@icloud.com>
2020-08-06 18:52:08 +0900

    Improve

--- editorconfig.fish
+++ editorconfig.fish
@@ -2,7 +2,7 @@ function editorconfig --description 'Generate editorconfig'
   if not test -e .editorconfig
     echo ".editorconfig not exist."
     echo "Creating .editorconfig."
-    cp $DOTFILES/.editorconfig-mimikun ./.editorconfig
+    cp $HOME/.editorconfig-template ./.editorconfig
   end
 end
 
